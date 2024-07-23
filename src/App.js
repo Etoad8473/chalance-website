26217b1042359b63bafd40d39fb80d480d0694c9
Ezra <ezrao@iastate.edu>
2024-07-22 21:03:09 -0500

    add next question button, separate question component into separate file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import Question from './Question';
 
 function App() {
 
@@ -8,68 +9,68 @@ function App() {
   const [chalanceScore, setChalanceScore] = useState(0);
 
   const questions = [
-    {question: 'I often suggest the groups plans',
-      chalance: 5,
+    {sentence: 'I often suggest the groups plans',
+      chalance: 1,
       casualness: 0
     },
-    {question: 'I do things even if I look dumb',
-      chalance: 5,
+    {sentence: 'I often do fun things even if I look dumb',
+      chalance: 1,
       casualness: 0
     },
-    {question: 'I do things others might consider embarrasing',
-      chalance: 5,
+    {sentence: 'I often do things others might consider embarrasing',
+      chalance: 1,
       casualness: 0
     },
-    {question: "Im always down to hang",
-      chalance: 5,
+    {sentence: "I'm always down to hang",
+      chalance: 1,
       casualness: 0
     },
-    {question: 'I will respond in the groupchat even if to let them know I cannot go',
-      chalance: 5,
+    {sentence: 'I will respond in the groupchat even if to let them know I cannot go',
+      chalance: 1,
       casualness: 0
     },
-    {question: 'seeing someone be bad at something (guitar) is embarrassing',
-      chalance: -5,
+    {sentence: 'seeing someone be bad at something (guitar) is embarrassing',
+      chalance: -1,
       casualness: 0
     },
-    {question: 'Ive tried learning a new skill in the last 3 months',
-      chalance: 5,
+    {sentence: 'Ive tried learning a new skill in the last 3 months',
+      chalance: 1,
       casualness: 0
     },
-    {question: 'My friends think Im spontaneous',
-      chalance: 5,
+    {sentence: 'My friends think Im spontaneous',
+      chalance: 1,
       casualness: 0
     },
-    {question: 'If youre not good at dancing, get out the circle',
-      chalance: -5,
+    {sentence: 'If youre not good at dancing, get out the circle',
+      chalance: -1,
       casualness: 0
     },
-    {question: 'People think Im judgemental',
-      chalance: -5,
+    {sentence: 'People think Im judgemental',
+      chalance: -1,
       casualness: 0
     },
-    {question: 'People think Im judgemental',
-      chalance: -5,
+    {sentence: 'People think Im judgemental',
+      chalance: -1,
       casualness: 0
     },
-    {question: 'Why would I do something Im bad at',
-      chalance: -5,
+    {sentence: 'Why would I do something Im bad at',
+      chalance: -1,
       casualness: 0
     },
-    {question: 'Ive been annoyed by people that think theyre better than me',
-      chalance: -5,
+    {sentence: 'Ive been annoyed by people that think theyre better than me',
+      chalance: -1,
       casualness: 0
     },
-    {question: 'Its more important to be realistic than to be positive',
-      chalance: -5,
+    {sentence: 'Its more important to be realistic than to be positive',
+      chalance: -1,
       casualness: 0
     },
-    {question: 'Its more important to be realistic than to be positive',
-      chalance: -5,
+    {sentence: 'Its more important to be realistic than to be positive',
+      chalance: -1,
       casualness: 0
     },
-    {question: 'The opposing political party is full of idiots',
-      chalance: -5,
+    {sentence: 'The opposing political party is full of idiots',
+      chalance: -1,
       casualness: 0
     }
 
@@ -90,51 +91,8 @@ function App() {
         <h1>
           Test
         </h1>
-        {Question(questions[questionIndex].question, questionIndex, chalanceScore, setChalanceScore)}
+        {Question(questions[questionIndex], questionIndex,setQuestionIndex, chalanceScore, setChalanceScore)}
         <button onClick={()=>{set(false)}}>Return home</button>
-      </div>
-    )
-  }
-
-  const Question = (title, qIndex, score, setScore) => {
-    return(
-      <div>
-
-        <form>
-          <label for="statement">Question {qIndex}: <br></br> {title}.</label><br></br><br></br>
-          
-          
-          <input type="radio"
-            id="sa"
-            name="statement"
-            value="5"
-          /><label for="sa">Strongly Agree</label><br></br>
-          
-          <input type="radio"
-            id="a"
-            name="statement"
-            value="2"
-          /><label for="a">Agree</label><br></br>
-          
-          <input type="radio"
-            id="n"
-            name="statement"
-            value="0"
-          /><label for="n">Neutral</label><br></br>
-          
-          <input type="radio"
-            id="d"
-            name="statement"
-            value="-2"
-          /><label for="d">Disagree</label><br></br>
-          
-          <input type="radio"
-            id="sd"
-            name="statement"
-            value="-5"
-          /><label for="sd">Strongly Disagree</label><br></br>
-          
-        </form>
       </div>
     )
   }
@@ -152,6 +110,7 @@ function App() {
       <button onClick={()=>{setQuestionIndex(questionIndex-1)}}>-</button>
       <p>{questionIndex}</p>
       <button onClick={()=>{setQuestionIndex(questionIndex+1)}}>+</button>
+      <p>Chalance Score: {chalanceScore}</p>
       
       {isTesting ? <Testing set = {setIsTesting}/> : <Home set = {setIsTesting}/> }
     </div>
