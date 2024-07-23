0edd309d4d505758e9ca8b2b7377960e732e5178
Ezra <ezrao@iastate.edu>
2024-07-23 14:55:56 -0500

    chalance score correctly updating

--- src/Question.js
+++ src/Question.js
@@ -3,52 +3,51 @@ import { useState, useEffect } from 'react';
 
 
 function Question (q, qIndex, setQIndex, score, setScore) {
-    return(
-      <div>
 
-        <form>
-          <label for="statement">Question {qIndex}: <br></br> {q.sentence}.</label><br></br><br></br>
-          
-          
-          <input type="radio"
-            id="sa"
-            name="statement"
-            value="5"
-          /><label for="sa">Strongly Agree</label><br></br>
-          
-          <input type="radio"
-            id="a"
-            name="statement"
-            value="2"
-          /><label for="a">Agree</label><br></br>
-          
-          <input type="radio"
-            id="n"
-            name="statement"
-            value="0"
-          /><label for="n">Neutral</label><br></br>
-          
-          <input type="radio"
-            id="d"
-            name="statement"
-            value="-2"
-          /><label for="d">Disagree</label><br></br>
-          
-          <input type="radio"
-            id="sd"
-            name="statement"
-            value="-5"
-          /><label for="sd">Strongly Disagree</label><br></br>
 
-          <button onClick={()=>{
-            setQIndex(qIndex+1)
-            // setScore(score + )
-          }}>Next Question</button>
+  return(
+    <div>
 
-        </form>
+      <form>
+        <label for="statement">Question {qIndex}: <br></br>
+         {q.sentence}.</label><br></br>
+         <br></br>
+        
+        
+        <input type="radio"
+          id="sa"
+          name="statement"
+          value="5"
+        /><label for="sa">Strongly Agree</label><br></br>
+        
+        <input type="radio"
+          id="a"
+          name="statement"
+          value="2"
+        /><label for="a">Agree</label><br></br>
+        
+        <input type="radio"
+          id="d"
+          name="statement"
+          value="-2"
+        /><label for="d">Disagree</label><br></br>
+        
+        <input type="radio"
+          id="sd"
+          name="statement"
+          value="-5"
+        /><label for="sd">Strongly Disagree</label><br></br>
 
-      </div>
-    )
+        <button onClick={()=>{
+            setQIndex(qIndex + 1)
+            let ansVal = parseInt(document.querySelector('input[name="statement"]:checked').value)
+            setScore(score + q.chalance * ansVal)
+        }}>Next Question</button>
+
+      </form>
+
+    </div>
+  )
 }
 
 
